46ad03eb653a98752ee2e0e66c8ab462782e6f87
done-n-dusted <31320.anurag@gmail.com>
2026-03-02 01:02:53 +0000

    Add linting dependencies and fix traverser logic

--- core/traverser.py
+++ core/traverser.py
@@ -1,24 +1,34 @@
+import os
+
 class Traverser:
     """Traverse directories and yield file paths.
 
     Example:
-        traverser = Traverser()
-        for path in traverser.iterate("/some/dir"):
+        traverser = Traverser("/some/dir", ignore_list=[".git", "__pycache__"])
+        for path in traverser.iterate():
             print(path)
     """
 
-    def __init__(self, follow_symlinks: bool = False):
+    def __init__(self, root_path: str, ignore_list: list[str] = None, follow_symlinks: bool = False):
+        self.root_path = root_path
+        self.ignore_list = ignore_list
         self.follow_symlinks = follow_symlinks
 
-    def iterate(self, root_path: str):
-        """Yield file paths under ``root_path``.
+    def _should_ignore(self, path: str) -> bool:
+        """Check if a path should be ignored."""
+        if self.ignore_list is None:
+            return False
+        for ignore_pattern in self.ignore_list:
+            if ignore_pattern in path:
+                return True
+        return False
 
-        Parameters
-        ----------
-        root_path: str
-            The directory to start traversal from.
-        """
-        import os
-        for dirpath, _, filenames in os.walk(root_path, followlinks=self.follow_symlinks):
-            for filename in filenames:
-                yield os.path.join(dirpath, filename)
+    def iterate(self):
+        """Yield file paths under ``root_path``."""
+        for root, dirs, files in os.walk(self.root_path, topdown=True, followlinks=self.follow_symlinks):
+            if self.ignore_list:
+                dirs[:] = [d for d in dirs if d not in self.ignore_list]
+                files = [f for f in files if f not in self.ignore_list]
+                
+            for filename in files:
+                yield os.path.join(root, filename)
